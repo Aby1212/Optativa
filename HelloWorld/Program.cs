d30262411ff20410dbeee41a854e8b6794a899cf
SARA RODR�GUEZ MAR�N <saralt00@gmail.com>
2025-10-17 21:52:17 -0500

    Cambio a Hello World

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Name = "Jhon";
+            Console.WriteLine($"Hello World! {Name}");
+            Console.WriteLine();
+        }
+    }
+}
+
+
